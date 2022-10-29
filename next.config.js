716ad9ae05dd6b66fd3630f47cbf578ff2030332
Achmad Zacky Rachmatullah <achmadzackyr@gmail.com>
2022-10-29 10:44:18 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_API_BACKEND: process.env.NEXT_PUBLIC_API_BACKEND,
+    NEXT_PUBLIC_AWS_S3: process.env.NEXT_PUBLIC_AWS_S3
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
